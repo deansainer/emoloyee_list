012d5a33b1ceb85bc61269081efde76a284436d7
deansainer <determind2k@gmail.com>
2023-01-25 00:01:57 +0100

    Initial commit

---
+++ employee_list/employee_app/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+
+
+admin.site.register(Location)
+admin.site.register(Position)
+admin.site.register(Employee)
+
---
+++ employee_list/employee_app/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+class Position(models.Model):
+    position_name = models.CharField(max_length=55)
+
+    def __str__(self):
+        return self.position_name
+
+
+class Location(models.Model):
+    location_name = models.CharField(max_length=55)
+
+    def __str__(self):
+        return self.location_name
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=55)
+    surname = models.CharField(max_length=85)
+    position_name = models.ForeignKey(Position, on_delete=models.CASCADE)
+    email = models.EmailField()
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    photo = models.ImageField()
+    birthday = models.DateField()
+    phone = models.CharField(max_length=12)
+
+    def __str__(self):
+        return ''.join(f'{self.name} {self.surname}')
