a2a4464dad417e08b09c646cbc33f01a18d1d902
deansainer <determind2k@gmail.com>
2023-01-26 14:29:45 +0100

    commit

--- employee_list/employee_app/urls.py
+++ employee_list/employee_app/urls.py
@@ -4,5 +4,6 @@ from .views import *
 
 
 urlpatterns = [
-    path('', views.employee_list)
+    path('', views.employee_list, name='employee_list'),
+    path('new_employee/', views.NewEmployee.as_view(), name='new_employee')
 ]
--- employee_list/employee_app/views.py
+++ employee_list/employee_app/views.py
@@ -1,10 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView
+
 from .models import *
+from .forms import *
 
 
 def employee_list(request):
     employees = Employee.objects.all()
     context = {'employees': employees}
     return render(request, 'employee_app/employee_list.html', context)
+
+
+class NewEmployee(TemplateView):
+    template_name = 'employee_app/new_employee.html'
+
+    def get(self, request, *args, **kwargs):
+        form = EmployeeForm()
+        context = {'form': form}
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        if request.method == 'POST':
+            form = EmployeeForm(request.POST)
+            if form.is_valid():
+                name = form.cleaned_data['name']
+                surname = form.cleaned_data['surname']
+                position_name = form.cleaned_data['position_name']
+                email = form.cleaned_data['email']
+                location = form.cleaned_data['location']
+                photo = form.cleaned_data['photo']
+                birthday = form.cleaned_data['birthday']
+                phone = form.cleaned_data['phone']
+
+                form.save()
+                return redirect('/')
+            context = {'form': form}
+            return render(request, self.template_name, context)
+
+
+
 
 
